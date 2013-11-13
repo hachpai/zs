7fd52d4479e5e0d132d8cc6ad9c1f701e13ff435
hachpai <wibautph@gmail.com>
2013-11-13 17:15:30 +0100

    Initial commit ZS

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zs::Application.config.secret_token = 'c39dab87547d9843f57173f2f4be077f347fca5ba397c7c54a82273991d0904c7c0b75ec01678c4b551d7d9848cd8fdbf716c8b780cb39bf3ce6f3d0af97e5cc'
